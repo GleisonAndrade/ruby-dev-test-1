a2dd51aa7e2c6cb829ce07c65a4a1e0c5100f03e
Gleison Andrade <gleisondeandradeesilva@gmail.com>
2025-05-27 23:47:59 -0300

    refactor: adjustments in storable

--- app/models/storable.rb
+++ app/models/storable.rb
@@ -1,6 +1,17 @@
-class Storable < ApplicationRecord
-  belongs_to :parent, class_name: 'Storable', optional: true
-  has_many :childrens, class_name: 'Storable', foreign_key: :parent_id
+# frozen_string_literal: true
 
-  validates :name, presence: true
+class Storable < ApplicationRecord
+  belongs_to :parent, class_name: 'DirectoryStorable', optional: true
+
+  validate :parent_must_be_directory_storable, if: -> { parent.present? }
+
+  validates :name, presence: true, uniqueness: { scope: :parent_id, case_sensitive: false }
+
+  private
+
+  def parent_must_be_directory_storable
+    unless parent.is_a?(DirectoryStorable)
+      errors.add(:parent, 'must be a DirectoryStorable')
+    end
+  end
 end
